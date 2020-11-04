15ce366582b680f199f7d6c6e52d23099d6097e1
Jordan (Tao Zhang) <zhangtbj@cn.ibm.com>
2020-11-04 14:54:54 +0800

    Update hello.js

--- nodejs/google/hello.js
+++ nodejs/google/hello.js
@@ -1,4 +1,1 @@
-exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!';
-  res.status(200).send(message);
-};
+module.exports = name => `Hello ${name}!`
