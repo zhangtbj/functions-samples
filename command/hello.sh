6baf5d8c0d44bbca255dd05e6c5b5abee0fa6e4c
Jordan (Tao Zhang) <zhangtbj@cn.ibm.com>
2020-11-09 16:08:44 +0800

    Update hello.sh

--- command/hello.sh
+++ command/hello.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-echo "Hostname : ${HOSTNAME}"
-echo "========================================="
-echo "Running WHOIS for : $1"
-echo "========================================="
-whois $1 |grep "^Name Server"
-echo "========================================="
-
-echo "Ping Check"
-echo "========================================="
-ping -c 1 $1
-echo "========================================="
+var=`cat`
+echo "Bash Hello ${var}"
