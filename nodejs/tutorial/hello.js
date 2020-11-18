1c20e93b7fc888241db6579455553ea7c413680e
Jordan (Tao Zhang) <zhangtbj@cn.ibm.com>
2020-11-18 21:49:37 +0800

    Update hello.js

--- nodejs/tutorial/hello.js
+++ nodejs/tutorial/hello.js
@@ -3,7 +3,7 @@ module.exports = function (params) {
     action = payload.action
     number = payload.issue.number;
     title = payload.issue.title;
-    id = "502J3DPUE/BQVJ2U62H/1aGAi63eZELMPB48bvasU47"
+    id = "502J3DPUE/B01EXET47AR/7uL8jEtX6o6wvMdguTTFIRya"
     console.log("A new Git issue %s action was token for issue %s: %s", action, number, title);
     
     console.log('Sending slack message');
@@ -11,7 +11,7 @@ module.exports = function (params) {
 
     var myJSONObject = {text: `A new Git issue ${action} action was token for issue ${number}: ${title}`};
     request({
-        url: `https://hooks.slack.com/services/${id.replace('5', 'T').replace('7', 'bs')}`,
+        url: `https://hooks.slack.com/services/${id.replace('5', 'T')}`,
         method: "POST",
         json: true, 
         body: myJSONObject
