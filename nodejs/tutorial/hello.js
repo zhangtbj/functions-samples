e461c74afa9fda73a4723ea485c9ca8dd8508055
Jordan (Tao Zhang) <zhangtbj@cn.ibm.com>
2020-11-18 17:27:34 +0800

    Update hello.js

--- nodejs/tutorial/hello.js
+++ nodejs/tutorial/hello.js
@@ -1,5 +1,4 @@
 module.exports = function (params) {
-    const axios = require('axios')
     let payload = params;
     action = payload.action
     number = payload.issue.number;
@@ -8,14 +7,16 @@ module.exports = function (params) {
     console.log("A new Git issue %s action was token for issue %s: %s", action, number, title);
     
     console.log('Sending slack message');
-    axios
-      .post(`https://hooks.slack.com/services/${id.replace('5', 'T').replace('7', 'bs')}`, {text: `A new Git issue ${action} action was token for issue ${number}: ${title}`})
-      .then(res => {
-        console.log(`statusCode: ${res.statusCode}`)
-        console.log(res)
-      })
-      .catch(error => {
-        console.error(error)
-      })
+    var request = require('request');
+
+    var myJSONObject = {text: `A new Git issue ${action} action was token for issue ${number}: ${title}`};
+    request({
+        url: `https://hooks.slack.com/services/${id.replace('5', 'T').replace('7', 'bs')}`,
+        method: "POST",
+        json: true, 
+        body: myJSONObject
+    }, function (error, response, body){
+        console.log(response);
+    });
     return {action: action, number:number, title:title};
 };
